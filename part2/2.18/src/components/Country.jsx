389bfc7d57f680217f55e85e44b006c107d3f168
Joni Ursin <joni.ursin@gmail.com>
2025-03-30 01:16:44 +0200

    2.20

--- part2/2.18/src/components/Country.jsx
+++ part2/2.18/src/components/Country.jsx
@@ -1,4 +1,16 @@
+const api_key = import.meta.env.VITE_SOME_KEY
+import { useState, useEffect } from "react";
+import axios from 'axios'
+
 const Country = ({country, onlyOne, setNewSearch}) => {
+  const [weather, setWeather] = useState(null);
+
+  const getWeather = () => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${api_key}&units=metric`).then((response) => {setWeather(response.data)})
+  }
+
+  useEffect(() => getWeather(), [])
+
   if (onlyOne) {
     return (
       <div>
@@ -8,6 +20,10 @@ const Country = ({country, onlyOne, setNewSearch}) => {
         <h2>Languages</h2>
         <div>{Object.values(country.languages).map(language => (<li key={language}>{language}</li>))}</div>
         <img src={country.flags.png} alt="Flag" />
+        <h2>Weather in {country.capital}</h2>
+        <div>Temperature {weather.main.temp} Celcius</div>
+        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="Weather icon" />
+        <div>Wind {weather.wind.speed} m/s</div>
       </div>
     )
   }
