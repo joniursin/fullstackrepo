110848bd133191e18a7aea7abff3070390bd15db
Joni Ursin <joni.ursin@gmail.com>
2025-05-16 03:21:47 +0300

    3.2

--- part3/3.1/index.js
+++ part3/3.1/index.js
@@ -28,6 +28,11 @@ app.get("/api/persons", (request, response) => {
     response.json(persons)
 }) 
 
+app.get("/info", (request, response) => {
+    response.write(`<div>Phonebook has info for ${persons.length} people</div>`)
+    response.end(new Date(Date.now()).toString())
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
