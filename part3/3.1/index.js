f33d15e9f9ef14ce7ebd1c6b02a60db299c7cac6
Joni Ursin <joni.ursin@gmail.com>
2025-05-16 19:59:03 +0300

    3.5

--- part3/3.1/index.js
+++ part3/3.1/index.js
@@ -23,6 +23,7 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+app.use(express.json())
 
 app.get("/api/persons", (request, response) => {
     response.json(persons)
@@ -48,6 +49,24 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id
   persons = persons.filter(person => person.id !== id)
   response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing"
+    })
+  }
+
+  const person = {
+    "id": String(Math.floor(Math.random() * 1000000)),
+    "name": body.name, 
+    "number": body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
