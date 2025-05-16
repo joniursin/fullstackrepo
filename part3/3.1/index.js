c0f725eaecbb11d114176048059886213a72f04f
Joni Ursin <joni.ursin@gmail.com>
2025-05-16 22:40:05 +0300

    3.8

--- part3/3.1/index.js
+++ part3/3.1/index.js
@@ -25,8 +25,13 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+morgan.token("body", req => {
+  return JSON.stringify(req.body)
+})
+
 app.use(express.json())
-app.use(morgan("tiny"))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
 app.get("/api/persons", (request, response) => {
     response.json(persons)
