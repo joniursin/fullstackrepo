b7028a0341d328aff86fdc4a5d363a9172c403c4
Joni Ursin <joni.ursin@gmail.com>
2025-05-16 19:28:59 +0300

    3.3

--- part3/3.1/index.js
+++ part3/3.1/index.js
@@ -33,6 +33,17 @@ app.get("/info", (request, response) => {
     response.end(new Date(Date.now()).toString())
 })
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
