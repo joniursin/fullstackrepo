d34a0d4515e50257bb59f2ff993f9b180699d6c8
Joni Ursin <joni.ursin@gmail.com>
2025-05-18 23:04:04 +0300

    3.9

--- part3/3.1/index.js
+++ part3/3.1/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 var morgan = require('morgan')
+const cors = require("cors")
 
 const app = express()
 
@@ -32,6 +33,8 @@ morgan.token("body", req => {
 
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+app.use(cors())
+app.use(express.static("dist"))
 
 app.get("/api/persons", (request, response) => {
     response.json(persons)
@@ -82,7 +85,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
 })
