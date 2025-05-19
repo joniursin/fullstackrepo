b75135873c0563fafab5cfe2eaeff1a907147707
Joni Ursin <joni.ursin@gmail.com>
2025-05-20 02:16:32 +0300

    3.16

--- part3/3.1/index.js
+++ part3/3.1/index.js
@@ -32,9 +32,18 @@ morgan.token("body", req => {
   return JSON.stringify(req.body)
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name == 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
-app.use(express.static("dist"))
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then(persons => {
@@ -49,19 +58,21 @@ app.get("/info", (request, response) => {
   })
 })
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+    if (person) {
+      response.json(person)
+    }
+    else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id).then(result => {
     response.status(204).end()
-  }).catch(error => {
-    console.log(error)
-    response.status(400).send({ error: 'malformatted id'})
-  })
+  }).catch(error => next(error))
 })
 
 app.post("/api/persons", (request, response) => {
@@ -71,12 +82,6 @@ app.post("/api/persons", (request, response) => {
       error: "name or number missing"
     })
   }
-  /*
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique"
-    })
-  }*/
 
   const person = new Person({
     "name": body.name, 
@@ -87,6 +92,13 @@ app.post("/api/persons", (request, response) => {
     response.json(savedPerson)
   })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
