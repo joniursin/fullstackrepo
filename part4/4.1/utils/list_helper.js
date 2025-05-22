18bb98db60699ece909041bf11b2aa56d9e36c36
Joni Ursin <joni.ursin@gmail.com>
2025-05-22 22:10:20 +0300

    4.5

--- part4/4.1/utils/list_helper.js
+++ part4/4.1/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
     return blogs.reduce((total, current) => total + current.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((total, current) => total.likes > current.likes ? total : current, 0)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
