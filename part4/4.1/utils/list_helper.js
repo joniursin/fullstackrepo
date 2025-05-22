61d7de1feeda7e587599adad0a990f1cc0de158b
Joni Ursin <joni.ursin@gmail.com>
2025-05-22 23:04:16 +0300

    4.6

--- part4/4.1/utils/list_helper.js
+++ part4/4.1/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -10,8 +12,15 @@ const favoriteBlog = (blogs) => {
     return blogs.reduce((total, current) => total.likes > current.likes ? total : current, 0)
 }
 
+const mostBlogs = (blogs) => {
+    if (!blogs.length) { return 0 }
+    const author = _.maxBy(Object.entries(_.countBy(blogs, 'author')), (max) => max)
+    return { author: author[0], blogs: author[1]}
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
