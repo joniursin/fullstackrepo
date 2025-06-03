c6e0ddb15645b3de3e3f49b3ff173c09be1f71c6
Joni Ursin <joni.ursin@gmail.com>
2025-06-04 00:49:58 +0300

    5.8

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
+import blogService from '../services/blogs'
 
 const Blog = (props) => {
   const [visible, setVisible] = useState(false)
+  const [likes, setLikes] = useState(props.blog.likes)
 
   const hideWhenVisible = { display: visible ? 'none' : '' }
   const showWhenVisible = { display: visible ? '' : 'none' }
 
   const toggleVisibility = () => {
     setVisible(!visible)
+  }
+
+  const addLike = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: props.blog.title,
+      author: props.blog.author,
+      url: props.blog.url,
+      likes: likes + 1,
+      user: props.blog.user.id
+    }
+    const response = await blogService.update(props.blog.id, blogObject)
+    setLikes(blogObject.likes)
   }
 
   return (
@@ -22,8 +37,8 @@ const Blog = (props) => {
           <button onClick={toggleVisibility}>hide</button>
           <p>{props.blog.url}</p>
           <p>
-            likes {props.blog.likes}
-            <button >like</button>
+            likes {likes}
+            <button onClick={addLike}>like</button>
           </p>
           <p>{props.blog.user.name}</p>
         </div>
