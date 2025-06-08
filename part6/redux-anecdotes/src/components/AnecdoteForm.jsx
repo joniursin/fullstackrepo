950cf48f8fd1d909add53650336f823d680630b0
Joni Ursin <joni.ursin@gmail.com>
2025-06-08 21:48:53 +0300

    6.8

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -12,11 +12,11 @@ const AnecdoteForm = () => {
 
   return (
     <div>
-        <h2>create new</h2>
-        <form onSubmit={addAnecdote}>
-            <div><input name="anecdote"/></div>
-            <button type="submit">create</button>
-        </form>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote"/></div>
+        <button type="submit">create</button>
+      </form>
     </div>
   )
 }
