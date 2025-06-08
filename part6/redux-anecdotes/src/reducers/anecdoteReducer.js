17d987abd3cda98b6a85c0f87044450bde30975a
Joni Ursin <joni.ursin@gmail.com>
2025-06-08 21:30:20 +0300

    6.6

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,11 +22,14 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
+  const addLikeToAnecdote = () => {
+    const findAnecdote = state.find(n => n.id === action.payload.id)
+    return { ...findAnecdote, votes: findAnecdote.votes + 1}
+  }
+
   switch(action.type) {
     case 'ADD_LIKE':
-      const findAnecdote = state.find(n => n.id === action.payload.id)
-      const changedAnecdote = { ...findAnecdote, votes: findAnecdote.votes + 1}
-      return state.map(a => a.id !== action.payload.id ? a : changedAnecdote)
+      return state.map(a => a.id !== action.payload.id ? a : addLikeToAnecdote())
     case 'NEW_ANECDOTE':
       return [...state, action.payload]
     default: return state
