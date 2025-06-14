cc52cada553b37fcccb2d64a431320461fc8a857
Joni Ursin <joni.ursin@gmail.com>
2025-06-15 01:13:59 +0300

    7.19

--- part7/bloglist-frontend/src/components/BlogView.jsx
+++ part7/bloglist-frontend/src/components/BlogView.jsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setLike } from "../reducers/blogReducer";
 import { initializeComments, newComment } from "../reducers/commentReducer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const BlogView = ({ blog }) => {
+  const [comment, setComment] = useState("");
   if (!blog) {
     return null;
   }
@@ -16,8 +17,10 @@ const BlogView = ({ blog }) => {
     dispatch(initializeComments(blog.id));
   }, []);
 
-  const handleComment = () => {
-    dispatch(newComment(blog.id, { content: "comment1" }));
+  const handleComment = (event) => {
+    event.preventDefault();
+    dispatch(newComment(blog.id, { content: comment }));
+    setComment("");
   };
 
   return (
@@ -32,7 +35,10 @@ const BlogView = ({ blog }) => {
       </p>
       <p>added by {blog.user.name}</p>
       <h3>comments</h3>
-      <button onClick={handleComment}>Post Comment</button>
+      <form onSubmit={handleComment}>
+        <input onChange={(event) => setComment(event.target.value)} />
+        <button type="submit">add comment</button>
+      </form>
       {comments.map((comment) => (
         <div key={comment._id}>
           <li>{comment.content}</li>
