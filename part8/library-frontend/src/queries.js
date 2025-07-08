f7601e132a0805cbd43a9aec2c6dfe1e27ca9a93
Joni Ursin <joni.ursin@gmail.com>
2025-07-09 01:18:44 +0300

    8.18

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -37,7 +37,9 @@ export const ADD_BOOK = gql`
     ) {
       title
       published
-      author
+      author {
+        name
+      }
       genres
     }
   }
@@ -48,6 +50,14 @@ export const EDIT_AUTHOR = gql`
     editAuthor(name: $name, setBornTo: $setBornTo) {
       name
       born
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
     }
   }
 `;
