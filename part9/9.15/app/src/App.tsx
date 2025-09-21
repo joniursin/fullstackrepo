a05ef99bdd3093203efe63bcd3737e31a2dd634b
Joni Ursin <joni.ursin@gmail.com>
2025-09-21 23:08:39 +0300

    9.16

--- part9/9.15/app/src/App.tsx
+++ part9/9.15/app/src/App.tsx
@@ -1,17 +1,81 @@
+import { partial } from "zod/mini";
+
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+  }
+
+  interface CoursePartDescription extends CoursePartBase {
+    description: string;
+  }
+
+  interface CoursePartBasic extends CoursePartDescription {
+    kind: "basic";
+  }
+
+  interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group";
+  }
+
+  interface CoursePartBackground extends CoursePartDescription {
+    backgroundMaterial: string;
+    kind: "background";
+  }
+
+  interface CoursePartRequirements extends CoursePartDescription {
+    requirements: string[];
+    kind: "special";
+  }
+
+  type CoursePart =
+    | CoursePartBasic
+    | CoursePartGroup
+    | CoursePartBackground
+    | CoursePartRequirements;
+
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial:
+        "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special",
     },
   ];
 
@@ -24,22 +88,58 @@ const App = () => {
     return <h1>{name}</h1>;
   };
 
-  interface Part {
-    name: string;
-    exerciseCount: number;
-  }
+  const Part = ({ part }: { part: CoursePart }) => {
+    switch (part.kind) {
+      case "basic":
+        return (
+          <div>
+            <h3>
+              {part.name} {part.exerciseCount}
+            </h3>
+            <p>{part.description}</p>
+          </div>
+        );
+      case "group":
+        return (
+          <div>
+            <h3>
+              {part.name} {part.exerciseCount}
+            </h3>
+            <p>project exercises {part.groupProjectCount}</p>
+          </div>
+        );
+      case "background":
+        return (
+          <div>
+            <h2>
+              {part.name} {part.exerciseCount}
+            </h2>
+            <p>{part.description}</p>
+            <p>background material: {part.backgroundMaterial}</p>
+          </div>
+        );
+      case "special":
+        return (
+          <div>
+            <h2>
+              {part.name} {part.exerciseCount}
+            </h2>
+            <p>{part.description}</p>
+            <p>required skills: {part.requirements.join(", ")}</p>
+          </div>
+        );
+    }
+  };
 
   interface ContentProps {
-    parts: Part[];
+    parts: CoursePart[];
   }
 
   const Content = ({ parts }: ContentProps) => {
     return (
       <div>
         {parts.map((part) => (
-          <p key={part.name}>
-            {part.name} {part.exerciseCount}
-          </p>
+          <Part key={part.name} part={part} />
         ))}
       </div>
     );
