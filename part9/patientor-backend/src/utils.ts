ee6901bbcbc0ae2c08fb55fc8cc4b7be638df295
Joni Ursin <joni.ursin@gmail.com>
2025-10-08 22:25:15 +0300

    9.23

--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,15 +1,49 @@
-import { NewPatient, Gender } from "./types";
+import { Gender, HealthCheckRating } from "./types";
 import { z } from "zod";
+
+const BaseEntrySchema = z.object({
+  id: z.string(),
+  description: z.string(),
+  date: z.date(),
+  specialist: z.string(),
+  diagnosisCodes: z.array(z.string()).optional(),
+});
+
+const HealthCheckEntrySchema = BaseEntrySchema.extend({
+  type: z.literal("HealthCheck"),
+  healthCheckRating: z.enum(HealthCheckRating),
+});
+
+const HospitalEntrySchema = BaseEntrySchema.extend({
+  type: z.literal("Hospital"),
+  discharge: z.object({
+    date: z.date(),
+    criteria: z.string(),
+  }),
+});
+
+const OccupationalHealthcareEntrySchema = BaseEntrySchema.extend({
+  type: z.literal("OccupationalHealthcare"),
+  employerName: z.string(),
+  sickLeave: z
+    .object({
+      startDate: z.date,
+      endDate: z.date(),
+    })
+    .optional(),
+});
+
+export const EntrySchema = z.discriminatedUnion("type", [
+  HospitalEntrySchema,
+  HealthCheckEntrySchema,
+  OccupationalHealthcareEntrySchema,
+]);
 
 export const newPatientSchema = z.object({
   name: z.string(),
   dateOfBirth: z.string().date(),
-  gender: z.nativeEnum(Gender),
+  gender: z.enum(Gender),
   occupation: z.string(),
   ssn: z.string(),
-  entries: z.array(z.object({})).default([]),
+  entries: z.array(EntrySchema).default([]),
 });
-
-export const toNewPatient = (object: unknown): NewPatient => {
-  return newPatientSchema.parse(object);
-};
